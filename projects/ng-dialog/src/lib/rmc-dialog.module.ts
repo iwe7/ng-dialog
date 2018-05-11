a439ae67c578b3c024b4ada1434e066d2e5a4e70
iwe7 <admin@meepo.com.cn>
2018-05-11 11:17:01 +0800

    更新

--- projects/ng-dialog/src/lib/rmc-dialog.module.ts
+++ projects/ng-dialog/src/lib/rmc-dialog.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DialogComponent } from "./dialog/dialog.component";
+
 import { DialogWrapDirective } from "./directives/dialog-wrap.directive";
 import { DialogTitleDirective } from "./directives/dialog-title.directive";
 import { DialogContentDirective } from "./directives/dialog-content.directive";
@@ -11,25 +12,22 @@ import { DialogFooterDirective } from "./directives/dialog-footer.directive";
 import { DialogDirective } from "./directives/dialog.directive";
 import { DialogMaskDirective } from "./directives/dialog-mask.directive";
 import { DialogCloseXDirective } from "./directives/dialog-close-x.directive";
-import { PipeNgModule } from 'lodash-pipes';
-import { IsTemplateRefPipe, IsTypePipe } from './pipe';
+export const directives = [
+  DialogWrapDirective,
+  DialogTitleDirective,
+  DialogContentDirective,
+  DialogCloseDirective,
+  DialogHeaderDirective,
+  DialogBodyDirective,
+  DialogFooterDirective,
+  DialogDirective,
+  DialogMaskDirective,
+  DialogCloseXDirective
+];
+import { IsTemplateRefPipe, IsTypePipe } from "./pipe";
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    DialogComponent,
-    DialogWrapDirective,
-    DialogTitleDirective,
-    DialogContentDirective,
-    DialogCloseDirective,
-    DialogHeaderDirective,
-    DialogBodyDirective,
-    DialogFooterDirective,
-    DialogDirective,
-    DialogMaskDirective,
-    DialogCloseXDirective,
-    IsTemplateRefPipe,
-    IsTypePipe
-  ],
+  declarations: [DialogComponent, ...directives, IsTemplateRefPipe, IsTypePipe],
   exports: [DialogComponent],
   entryComponents: [DialogComponent]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import { NgModule } from "@angular/core";
-
 import { AppComponent } from "./app.component";
 import { RmcDialogModule } from "ng-dialog";
 import { TestDialogComponent } from "./test-dialog/test-dialog.component";
